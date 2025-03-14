6024161ab98b5a14744cc408fb7dbf78ddafffa3
Pegui <Pegui@ENG-PEDROKEKO>
2025-03-14 15:32:15 +0100

    Adicionar arquivos de projeto.

---
+++ SIG_PSPEP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SIG_PSPEP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
